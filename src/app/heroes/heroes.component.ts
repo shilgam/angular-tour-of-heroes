fdff4b4cfeb2a12db801f85d58296463cfc0d36e
Ilgam Shakirov <shilgam@gmail.com>
2021-01-13 16:24:49 +0300

    Add ability to edit the hero name in an <input> textbox
    NOTES:
    - use two-way data binding with the ngModel directive
    - import the FormsModule in the AppModule so that Angular would recognize and apply the ngModel directive

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +10,7 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  };
 
   constructor() { }
 
