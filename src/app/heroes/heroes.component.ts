34d794f4dac249172f7e08434b831d5a64631224
Ilgam Shakirov <shilgam@gmail.com>
2021-01-14 14:18:49 +0300

    Add a HeroService to get hero data
    NOTES:
    - registered the HeroService as the provider of its service so that it can be injected
    - used Angular Dependency Injection to inject it into a component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
-
   selectedHero: Hero | undefined;
 
-  constructor() { }
+  heroes: Hero[] | undefined;
+
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
