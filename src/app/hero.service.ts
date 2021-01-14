b19d483820077f934e9e33c208e22c2960306a3b
Ilgam Shakirov <shilgam@gmail.com>
2021-01-14 14:42:41 +0300

    Get heroes data asynchronously to not block user while the service waits
    NOTES:
    - gave the HeroService get data method an asynchronous signature
    - discovered Observable and the RxJS Observable library
    - used RxJS of() to return an observable of mock heroes (Observable<Hero[]>)
    - The component's ngOnInit lifecycle hook calls the HeroService method, not the constructor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -9,7 +11,7 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
   constructor() { }
 
-  getHeroes(): Hero[] {
-  return HEROES;
-}
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
 }
