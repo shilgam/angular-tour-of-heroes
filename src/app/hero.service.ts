d5885b0fb8d91028df77b727f279b70d2a29661a
Ilgam Shakirov <shilgam@gmail.com>
2021-01-19 15:25:54 +0300

    HttpClient: Get heroes with HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
@@ -10,17 +11,28 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero | undefined> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
