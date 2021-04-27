7fa3296e3a4b9b78d0aeb9dead7654e1a8119133
Ilgam Shakirov <shilgam@gmail.com>
2021-04-27 13:25:56 +0300

    FormGroup | Test address.component.spec
    Src: https://www.telerik.com/blogs/testing-dynamic-forms-in-angular

--- src/app/address/address.component.spec.ts
+++ src/app/address/address.component.spec.ts
@@ -27,11 +27,58 @@ describe('AddressComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the form', () => {
+    component.profileForm.patchValue({
+      address: {
+        street: '123 Drew Street',
+        city: 'San Francisco',
+        state: 'CA',
+        zip: '90290'
+      }
+    });
+
+    const compiled = fixture.debugElement.nativeElement;
+    const streetInput = compiled.querySelector('input[formcontrolname="street"]');
+    const cityInput = compiled.querySelector('input[formcontrolname="city"]');
+    const stateInput = compiled.querySelector('input[formcontrolname="state"]');
+    const zipInput = compiled.querySelector('input[formcontrolname="zip"]');
+
+    fixture.detectChanges();
+
+    expect(streetInput.value).toEqual('123 Drew Street');
+    expect(cityInput.value).toEqual('San Francisco');
+    expect(stateInput.value).toEqual('CA');
+    expect(zipInput.value).toEqual('90290');
+  });
+
+  it('should set proper validity state of address and display in the form', () => {
+    const form = component.profileForm;
+
+    expect(form.valid).toBeFalsy();
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.innerText).toContain('Address isValid: false');
+
+    const streetInput = form.controls.address.get('street');
+    streetInput.setValue('123 Drew Street');
+
+    expect(form.valid).toBeTruthy();
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.innerText).toContain('Address isValid: true');
+  });
 });
 
 @Component({
-  selector: 'app-profile-editor',
-  template: '<app-address [parentFormGroup]="profileForm" #address></app-address>'
+  selector: 'app-test-host-component',
+  template: `<form [formGroup]="profileForm">
+      <app-address [parentFormGroup]="profileForm" #address></app-address>
+    </form>
+    <p>Address isValid: {{ address.isValid }}</p>
+    <p>Address isDirty: {{ address.isDirty }}</p> `
 })
 class TestHostComponent {
   constructor(private fb: FormBuilder) {}
